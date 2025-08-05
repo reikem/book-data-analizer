f25795b6878b51d8528260c626a024fe470b7483
reikemdev <jimmymeneses11@gmail.com>
2025-08-05 18:16:53 -0400

    update config

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Providers } from './providers/Providers.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import "./index.css"
+import App from "./App"
+import { Providers } from "./providers/Providers"
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Providers>
+        <App />
+      </Providers>
+    </BrowserRouter>
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import path from "path"
 
 export default defineConfig({
-  base: "/book-data-analizer/",
+  base: '/book-data-analizer/',
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
